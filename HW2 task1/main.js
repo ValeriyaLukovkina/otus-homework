62d98b1490bb726865d3590268d2b19a4c4b4a02
Valeriya <valeriyalukovkina@gmail.com>
2022-08-02 00:44:04 +0800

    ready hw2

--- HW2 task1/main.js
+++ HW2 task1/main.js
@@ -1,9 +1,3 @@
-function promiseReduce(asyncFunctions, reduce, initialValue) {
-    new Promise (function (resolve, reject) {
-        resolve(reduce)
-    })
-}
-
 var fn1 = () => {
     console.log('fn1')
     return Promise.resolve(1)
@@ -12,3 +6,24 @@ var fn2 = () => new Promise(resolve => {
     console.log('fn2')
     setTimeout(() => resolve(2), 1000)
 })
+
+async function promiseReduce(asyncFunctions, reduce, initialValue = 0) {
+    let result = initialValue;
+    for (i = 0; i < asyncFunctions.length; i++) {
+        try {
+            result = reduce (result, await asyncFunctions[i]())
+        } catch (err) {
+            throw err;
+        }
+    }
+    return Promise.resolve(result);
+}
+promiseReduce(
+    [fn1, fn2],
+    function (memo, value) {
+    console.log('reduce')
+    return memo * value
+    },
+    1
+    )
+    .then(console.log)
